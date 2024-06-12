d4051400bd343b2742c3b4ab70e4be3570189ca2
balajispero <119649215+balajispero@users.noreply.github.com>
2024-06-12 16:01:07 +0530

    work on config file
    work on config file

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/rehab_global/';
+$config['base_url']	= 'http://localhost/Assisted_Living/';
 /*
 |--------------------------------------------------------------------------
 | Index File
